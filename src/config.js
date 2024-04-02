8fd16c34d3ae2f9c4267091218fac0896e2182e6
saipavan168 <potnuru.pavan168@gmail.com>
2024-04-02 14:44:54 +0530

    pushing clothing APP

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const produtsServiceLocalHost = 'http://localhost:8132/api'
 const usersServiceLocalHost = 'http://localhost:8232/api'
 
-const produtsServiceDevHost = 'http://34.125.84.228:5132/api'
-const usersServiceDevHost = 'http://34.125.84.228:5232/api'
+const produtsServiceDevHost = 'http://34.69.91.250:5132/api'
+const usersServiceDevHost = 'http://34.69.91.250:5232/api'
 
 const produtsServiceTestHost = ''
 const usersServiceTestHost = ''
